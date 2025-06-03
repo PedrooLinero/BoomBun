01131958e4ac813dd28ccce16c2f762c4ebd0ee3
Pedro Linero <xplinari721@ieshnosmachado.org>
2025-06-03 08:27:31 +0200

    commit

--- frontend/src/pages/config.js
+++ frontend/src/pages/config.js
@@ -1,3 +1,3 @@
-export const apiUrl = import.meta.env.VITE_API_URL || 'https://boombun-production.up.railway.app/api';
-export const staticUrl = import.meta.env.VITE_STATIC_URL || 'https://boombun-production.up.railway.app';
+export const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
+export const staticUrl = import.meta.env.VITE_STATIC_URL || 'http://localhost:3000';
 export const appTitle = import.meta.env.VITE_APP_TITLE || 'FrascoHosteleros';
